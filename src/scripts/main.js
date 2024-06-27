c288380deb0e947fe318fbd17ba6a1900d5205a0
= <=>
2024-06-27 23:41:09 +0300

    list added

---
+++ src/scripts/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const food = {
+  'Drink': {
+    'Wine': {},
+    'Schnaps': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.getElementById('tree');
+
+const createTree = (element, data) => {
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+    li.textContent = key;
+
+    if (Object.keys(data[key].length > 0)) {
+      createTree(li, data[key]);
+    }
+
+    ul.appendChild(li);
+  }
+
+  element.appendChild(ul);
+}
+createTree(tree, food);
